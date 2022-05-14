fa577f3dcc20dcef1409be9b613455244ec8bf17
lyj <jccbstc@outlook.com>
2022-05-14 16:36:46 +0800

    third commit

--- src/main/java/com/lyj/git.java
+++ src/main/java/com/lyj/git.java
@@ -4,5 +4,6 @@ public class git {
     public static void main(String[] args) {
         System.out.print("Hello Git!");
         System.out.print("Hello Git!");
+        System.out.print("Hello Git!");
     }
 }
