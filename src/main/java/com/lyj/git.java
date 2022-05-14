14d9058816c5c727ddb1a56f3d2ffc533ada175a
lyj <jccbstc@outlook.com>
2022-05-14 17:20:10 +0800

    push commit

--- src/main/java/com/lyj/git.java
+++ src/main/java/com/lyj/git.java
@@ -4,10 +4,7 @@ public class git {
     public static void main(String[] args) {
         System.out.print("Hello Git!");
         System.out.print("Hello Git!");
-        System.out.print("Hello Git!");
-        System.out.print("55555555");
-        System.out.print("444444444");
 
-        System.out.println("");
+        System.out.print("push");
     }
 }
