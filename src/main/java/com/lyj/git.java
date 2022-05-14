bfd00efd96ee44092ad9cf3c120a6950ba2b6958
lyj <jccbstc@outlook.com>
2022-05-14 16:42:31 +0800

    hot-fix first

--- src/main/java/com/lyj/git.java
+++ src/main/java/com/lyj/git.java
@@ -4,5 +4,7 @@ public class git {
     public static void main(String[] args) {
         System.out.print("Hello Git!");
         System.out.print("Hello Git!");
+        System.out.print("Hello Git!");
+        System.out.print("444444444");
     }
 }
